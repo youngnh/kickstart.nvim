eeb651af33b530233accbc836d092156f25bb346
Nate Young <nathaniel.young@mpls.frb.org>
2025-10-14 11:30:24 -0500

    set tabstops to 2, down from 4

--- init.lua
+++ init.lua
@@ -170,9 +170,9 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 
 -- tab/spaces/indentation-related
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
