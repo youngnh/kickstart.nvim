d21152fe6c9e4e5b1405a35649bf3937c9df4f54
Nate Young <youngnh@gmail.com>
2025-11-11 09:36:03 -0600

    move clojure-related plugins into one file

---
+++ lua/custom/plugins/clojure.lua
@@ -0,0 +1,14 @@
+-- Building a fully-featured Clojure IDE
+-- building on: https://endot.org/2023/05/27/vim-clojure-dev-2023/
+
+return {
+  -- Clojure syntax highlighting and indentation
+  -- https://github.com/guns/vim-clojure-static
+  'guns/vim-clojure-static',
+
+  -- structural editing
+  'guns/vim-sexp',
+  'tpope/vim-sexp-mappings-for-regular-people',
+  'tpope/vim-repeat',
+  'tpope/vim-surround',
+}
--- lua/custom/plugins/vim-clojure-static.lua
+++
@@ -1,6 +0,0 @@
--- Clojure syntax highlighting and indentation
--- https://github.com/guns/vim-clojure-static
-
-return {
-  'guns/vim-clojure-static',
-}
